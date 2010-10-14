bedba4d1f720f047cbacbf1afaed18baaf636e4d
Velina Veleva <vvveleva@gmail.com>
2010-10-14 03:49:59 -0400

    Resized the text area in the comment box.

--- static/js/commentable.js
+++ static/js/commentable.js
@@ -62,7 +62,7 @@ if (typeof Commentable === "undefined") {
                 },
                 init: function () {
                     CONFIG.$comment_dialog.tabs();
-                    CONFIG.$comment_dialog.dialog({autoOpen: false, minWidth: 520});
+                    CONFIG.$comment_dialog.dialog({autoOpen: false, minWidth: 450});
                     add_comment_boxes();
                 }
             };
